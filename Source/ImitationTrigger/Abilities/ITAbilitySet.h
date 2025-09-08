7f388b67860206063539ed7764e8a227cbd77b58
DaegeunChoe <asdf12gg@gmail.com>
2025-09-08 10:32:46 +0900

    PawnData 생성, AbilitySet, InputConfig, CemeraMode 껍데기 클래스 생성

---
+++ Source/ImitationTrigger/Abilities/ITAbilitySet.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "ITAbilitySet.generated.h"
+
+
+UCLASS()
+class IMITATIONTRIGGER_API UITAbilitySet : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ImitationTrigger/Character/ITPawnData.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Engine/DataAsset.h"
+#include "ITPawnData.generated.h"
+
+class UITAbilitySet;
+class UITInputConfig;
+class UITCameraMode;
+
+/**
+ * Pawn을 정의하기 위한 속성(properties)를 가지고 있는 Data Asset
+ * ULyraPawnData의 구성을 참고함
+ */
+UCLASS(BlueprintType, Const)
+class IMITATIONTRIGGER_API UITPawnData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UITPawnData(const FObjectInitializer& ObjectInitializer);
+
+	// Pawn이나 Character에 부여할(grand) Ability 정보
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PawnData|Abilities")
+	TArray<TObjectPtr<UITAbilitySet>> AbilitySets;
+
+	// Pawn이나 Character에 부여할(grand) Ability 정보
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PawnData|Abilities")
+	TObjectPtr<UITInputConfig> InputConfig;
+
+	// Pawn이나 Character에 부여할(grand) Ability 정보
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PawnData|Abilities")
+	TSubclassOf<UITCameraMode> DefaultCameraMode;
+};

48ef98bd04907dfab37eeefd38312fdeeb40e260
DaegeunChoe <asdf12gg@gmail.com>
2025-09-08 20:26:15 +0900

    ASC는 PlayerState에 위치해야 함

--- Source/ImitationTrigger/Player/ITPlayerState.cpp
+++ Source/ImitationTrigger/Player/ITPlayerState.cpp
@@ -1,2 +1,30 @@
 #include "Player/ITPlayerState.h"
+#include "Player/ITPlayerController.h"
+#include "Character/ITCharacter.h"
+#include "AbilitySystem/ITAbilitySystemComponent.h"
 
+AITPlayerState::AITPlayerState(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	AbilitySystemComponent = ObjectInitializer.CreateDefaultSubobject<UITAbilitySystemComponent>(this, TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	// AbilitySystem 네트워크 관련: needs to be updated at a high frequency.
+	SetNetUpdateFrequency(100.0f);
+}
+
+AITPlayerController* AITPlayerState::GetITPlayerController() const
+{
+	return Cast<AITPlayerController>(GetOwner());
+}
+
+AITCharacter* AITPlayerState::GetITCharacter() const
+{
+	return Cast<AITCharacter>(GetPawn());
+}
+
+UAbilitySystemComponent* AITPlayerState::GetAbilitySystemComponent() const
+{
+	return GetLyraAbilitySystemComponent();
+}

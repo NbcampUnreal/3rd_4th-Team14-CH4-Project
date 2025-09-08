53a30c61f3bb0738984a792d6d18f04a113c1d1a
DaegeunChoe <asdf12gg@gmail.com>
2025-09-08 16:48:45 +0900

    input config 구현

--- Source/ImitationTrigger/Input/ITInputConfig.cpp
+++ Source/ImitationTrigger/Input/ITInputConfig.cpp
@@ -1,2 +1,42 @@
 #include "Input/ITInputConfig.h"
 
+UITInputConfig::UITInputConfig(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+const UInputAction* UITInputConfig::FindNativeInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const FITInputAction& Action : NativeInputActions)
+	{
+		if (Action.InputAction && (Action.InputTag == InputTag))
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find NativeInputAction for InputTag [%s] on InputConfig [%s]."), *InputTag.ToString(), *GetNameSafe(this));
+	}
+
+	return nullptr;
+}
+
+const UInputAction* UITInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const FITInputAction& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && (Action.InputTag == InputTag))
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s] on InputConfig [%s]."), *InputTag.ToString(), *GetNameSafe(this));
+	}
+
+	return nullptr;
+}
--- Source/ImitationTrigger/Input/ITInputConfig.h
+++ Source/ImitationTrigger/Input/ITInputConfig.h
@@ -1,11 +1,44 @@
 #pragma once
 
 #include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
 #include "ITInputConfig.generated.h"
 
-UCLASS()
+class UInputAction;
+
+// Pair of (Gameplay Tag, Input Action)
+USTRUCT(BlueprintType)
+struct FITInputAction
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<const UInputAction> InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+};
+
+
+UCLASS(BlueprintType, Const)
 class IMITATIONTRIGGER_API UITInputConfig : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
+	UITInputConfig(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, Category = "Lyra|Pawn")
+	const UInputAction* FindNativeInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = true) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Lyra|Pawn")
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = true) const;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FITInputAction> NativeInputActions;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FITInputAction> AbilityInputActions;
 };

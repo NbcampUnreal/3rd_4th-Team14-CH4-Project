1ace2ad0ffa3888e6867932a0ddd72f1286ed256
DaegeunChoe <asdf12gg@gmail.com>
2025-09-16 17:57:22 +0900

    로그 관련 내용만 추가

--- Source/ImitationTrigger/System/ITLogChannel.cpp
+++ Source/ImitationTrigger/System/ITLogChannel.cpp
@@ -1,1 +1,14 @@
 #include "System/ITLogChannel.h"
+
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+//#include "ImitationTrigger.h"
+//#include "Modules/ModuleManager.h"
+
+// IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ImitationTrigger, "ImitationTrigger");
+
+#pragma region NetLogging
+
+DEFINE_LOG_CATEGORY(LogITNet);
+
+#pragma endregion

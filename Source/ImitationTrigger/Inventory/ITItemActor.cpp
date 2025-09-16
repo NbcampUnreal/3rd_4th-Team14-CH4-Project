cc735bcfaa499711ad64d0e5ea2764f3b7941d06
Jak-Jang <jjgk24211@gmail.com>
2025-09-16 12:15:46 +0900

    ItemInstance, ItemActor 추가
    ItemInstance: 'UObject'
    - 런타임에 존재하는 아이템 인스턴스 클래스
    
    ItemActor: 'AActor'
    - 트랜스폼과 메시를 가진 아이템 액터

---
+++ Source/ImitationTrigger/Inventory/ITItemActor.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ITItemActor.h"
+#include "ITItemDefinition.h"
+#include "ITItemInstance.h"
+#include "Components/SphereComponent.h"
+#include "Net/UnrealNetwork.h"
+
+AITItemActor::AITItemActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	SetRootComponent(StaticMeshComponent);
+	StaticMeshComponent->SetIsReplicated(true);
+	StaticMeshComponent->SetCollisionProfileName(TEXT("PhysicsActor"));
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(RootComponent);
+	SphereComponent->SetSphereRadius(100.0f);
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+}
+
+void AITItemActor::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AITItemActor, ItemInstance);
+}
+
+void AITItemActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority() && ItemInstance == nullptr && PlacedItemDefinition != nullptr)
+	{
+		UITItemInstance* NewItemInstance = NewObject<UITItemInstance>(this);
+		NewItemInstance->InitItemInstance(PlacedItemDefinition, PlacedItemQuantity);
+		InitItemActor(NewItemInstance);
+	}
+}
+
+UITItemInstance* AITItemActor::GetItemInstance() const
+{
+	return ItemInstance;
+}
+
+void AITItemActor::InitItemActor(UITItemInstance* InitItemInstance)
+{
+	if (HasAuthority())
+	{
+		ItemInstance = InitItemInstance;
+		UpdateAppearance();
+	}
+}
+
+void AITItemActor::UpdateAppearance()
+{
+	if (ItemInstance == nullptr || ItemInstance->ItemDefinition == nullptr)
+	{
+		return;
+	}
+
+	const UITItemDefinition* ItemDefinition = ItemInstance->ItemDefinition;
+	if (UStaticMesh* NewMesh = ItemDefinition->ItemMesh.LoadSynchronous())
+	{
+		StaticMeshComponent->SetStaticMesh(NewMesh);
+	}
+	for (int32 i = 0; i < ItemDefinition->ItemMaterial.Num(); i++)
+	{
+		if (UMaterialInterface* NewMaterial = ItemDefinition->ItemMaterial[i].LoadSynchronous())
+		{
+			StaticMeshComponent->SetMaterial(i, NewMaterial);
+		}
+	}
+}
+
+void AITItemActor::OnRep_ItemInstance()
+{
+	UpdateAppearance();
+}
---
+++ Source/ImitationTrigger/Inventory/ITItemInstance.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ITItemInstance.h"
+#include "Net/UnrealNetwork.h"
+
+bool UITItemInstance::IsSupportedForNetworking() const
+{
+	return true;
+}
+
+void UITItemInstance::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	UObject::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UITItemInstance, ItemDefinition);
+	DOREPLIFETIME(UITItemInstance, ItemQuantity);
+}
+
+void UITItemInstance::InitItemInstance(UITItemDefinition* InItemDefinition, int32 InitQuantity)
+{
+	ItemDefinition = InItemDefinition;
+	ItemQuantity = InitQuantity;
+}

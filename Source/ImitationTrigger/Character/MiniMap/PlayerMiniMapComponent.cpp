20645935e9c869391fc06cc86f65b742df449017
prostruin <wkd83555@naver.com>
2025-09-17 20:35:37 +0900

    MiniMap & Map

--- Source/ImitationTrigger/Character/MiniMap/PlayerMiniMapComponent.cpp
+++ Source/ImitationTrigger/Character/MiniMap/PlayerMiniMapComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "UI/MiniMap/PlayerMiniMapComponent.h"
+#include "Character/MiniMap/PlayerMiniMapComponent.h"
 
 #include "Components/SceneCaptureComponent2D.h"
 #include "Engine/StaticMeshActor.h"

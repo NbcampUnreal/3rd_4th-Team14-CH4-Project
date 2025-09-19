64ef6027afa96dde5482033cc8078b4ab1ecc156
장재근 <jjgk24211@gmail.com>
2025-09-19 12:32:20 +0900

    Feature/inventory/item (#27)
    * ItemDefinition, ItemFragment 추가
    * ItemInstance, ItemActor 추가
    * ItemTag, ItemDefinition 헬퍼 함수 추가, 폴더 구조 변경, 구조체 명칭 수정
    * Item Tag 적용, Tag를 이용한 함수 추가
    * Item Fragment, Definition Getter 추가
    * Test용 애셋 추가
    * fix: 리뷰 수정 사항 반영
    * fix: 아이템 네이티브 태그 수정
    * Test용 애셋 수정
    * test용 애셋 최신화
    * fix: Test용 GameplayEffect 수정
    * fix: 리뷰 수정 사항 반영
    * fix: 프래그먼트 태그 메타지정자 수정

---
+++ Source/ImitationTrigger/Item/ITItemActor.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ITItemActor.h"
+#include "ITItemDefinition.h"
+#include "ITItemInstance.h"
+#include "Components/SphereComponent.h"
+#include "Net/UnrealNetwork.h"
+
+AITItemActor::AITItemActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	SetRootComponent(StaticMeshComponent);
+	StaticMeshComponent->SetIsReplicated(true);
+	StaticMeshComponent->SetCollisionProfileName(TEXT("PhysicsActor"));
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(RootComponent);
+	SphereComponent->SetSphereRadius(100.0f);
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+}
+
+void AITItemActor::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AITItemActor, ItemInstance);
+}
+
+void AITItemActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		if (ItemInstance == nullptr && PlacedItemDefinition != nullptr)
+		{
+			UITItemInstance* NewItemInstance = NewObject<UITItemInstance>(this);
+			NewItemInstance->InitItemInstance(PlacedItemDefinition, PlacedItemQuantity);
+			InitItemActor(NewItemInstance);
+		}
+	}
+}
+
+UITItemInstance* AITItemActor::GetItemInstance() const
+{
+	return ItemInstance;
+}
+
+void AITItemActor::InitItemActor(UITItemInstance* InitItemInstance)
+{
+	if (HasAuthority())
+	{
+		ItemInstance = InitItemInstance;
+		UpdateAppearance();
+	}
+}
+
+void AITItemActor::UpdateAppearance()
+{
+	if (ItemInstance == nullptr || ItemInstance->ItemDefinition == nullptr)
+	{
+		return;
+	}
+
+	const UITItemDefinition* ItemDefinition = ItemInstance->ItemDefinition;
+	if (ItemDefinition->ItemMesh)
+	{
+		StaticMeshComponent->SetStaticMesh(ItemDefinition->ItemMesh);
+	}
+	for (int32 i = 0; i < ItemDefinition->ItemMaterial.Num(); i++)
+	{
+		if (ItemDefinition->ItemMaterial[i])
+		{
+			StaticMeshComponent->SetMaterial(i, ItemDefinition->ItemMaterial[i]);
+		}
+	}
+}
+
+void AITItemActor::OnRep_ItemInstance()
+{
+	UpdateAppearance();
+}
---
+++ Source/ImitationTrigger/Item/ITItemInstance.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ITItemInstance.generated.h"
+
+class UITItemDefinition;
+
+UCLASS(BlueprintType)
+class IMITATIONTRIGGER_API UITItemInstance : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool IsSupportedForNetworking() const override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void InitItemInstance(UITItemDefinition* InItemDefinition, int32 InitQuantity = 1);
+
+	UITItemDefinition* GetItemDefinition() const;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated, Category = "Item")
+	TObjectPtr<UITItemDefinition> ItemDefinition;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated, Category = "Item")
+	int32 ItemQuantity = 0;
+};

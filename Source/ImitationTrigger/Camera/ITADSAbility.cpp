519247030e8a24fb4bacb5dd1d91d5b61bf2967b
DaegeunChoe <asdf12gg@gmail.com>
2025-09-23 19:12:51 +0900

    ADS 어빌리티에서 사용하지않는 함수 제거

--- Source/ImitationTrigger/Camera/ITADSAbility.cpp
+++ Source/ImitationTrigger/Camera/ITADSAbility.cpp
@@ -6,24 +6,9 @@ UITADSAbility::UITADSAbility()
 	ActivationOwnedTags.AddTag(ITCameraModeTags::CameraMode_ADS);
 }
 
-bool UITADSAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const
-{
-	return Super::CanActivateAbility(Handle, ActorInfo, SourceTags, TargetTags, OptionalRelevantTags);
-}
-
-void UITADSAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* OwnerInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
-{
-	Super::ActivateAbility(Handle, OwnerInfo, ActivationInfo, TriggerEventData);
-}
-
 void UITADSAbility::InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
 {
 	Super::InputReleased(Handle, ActorInfo, ActivationInfo);
 
 	CancelAbility(Handle, ActorInfo, ActivationInfo, true);
-}
-
-void UITADSAbility::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
-{
-	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
 }
